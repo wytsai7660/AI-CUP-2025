c08f2dc50bf0fd48df49942ab97e4db98aea251b
wytsai7660 <wytsai7660@gmail.com>
2025-05-12 13:12:15 +0800

    Add a dedicated configuration file `config.py`

---
+++ config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import sys
+
+
+# ===== Paths =====
+BASE_PATH = Path(__file__).parent
+
+TRAIN_DATA_DIR = BASE_PATH / "39_Training_Dataset" / "train_data"
+TRAIN_INFO = BASE_PATH / "39_Training_Dataset" / "train_info.csv"
+
+TEST_DATA_DIR = BASE_PATH / "39_Test_Dataset" / "test_data"
+TEST_INFO = BASE_PATH / "39_Test_Dataset" / "test_info.csv"
+
+# Check if any of the paths are absent
+for path in [TRAIN_DATA_DIR, TRAIN_INFO, TEST_DATA_DIR, TEST_INFO]:
+    if not path.exists():
+        sys.exit(f"{path}: No such file or directory")
+
+# ===== Constants =====
+POSSIBLE_VALUES = {
+    "gender": [1, 2],
+    "hand": [1, 2],
+    "years": [0, 1, 2],
+    "level": [2, 3, 4, 5],
+}
+
+print("OK")  # TODO: remove
