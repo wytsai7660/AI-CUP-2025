287386acfb1e41638ed72a39a490d1f832d74c4c
YueLin03 <411410014@alum.ccu.edu.tw>
2025-05-20 12:44:26 +0800

    Fix: turn tensor into np array before further processing

--- helper/segment.py
+++ helper/segment.py
@@ -10,6 +10,7 @@ from scipy.signal import find_peaks
 from scipy.signal import detrend
 from scipy.signal import butter, filtfilt
 from sklearn.preprocessing import MinMaxScaler
+import warnings
 
 """
 The segmenting method to apply to the data
@@ -183,18 +184,19 @@ class IntergratedSplit(Segment):
 
     如果偵測到的揮拍次數不夠，則使用Yungan的分段方法
     """
-    
 
     def __init__(self):
         pass
-    
+
+    @staticmethod
     def lowpass_filter(data, cutoff=5, fs=85, order=4):
         nyq = 0.5 * fs
         normal_cutoff = cutoff / nyq
         b, a = butter(order, normal_cutoff, btype='low', analog=False)
         return filtfilt(b, a, data)
 
-    def find_k(waveform, crossings_num = 54, step=0.001):
+    @staticmethod
+    def find_k(waveform, crossings_num=54, step=0.001):
         max_y = 1.0
         min_y = 0.0
         k = max_y
@@ -207,19 +209,17 @@ class IntergratedSplit(Segment):
             n_crossings = len(crossings)
 
             if n_crossings >= crossings_num:
-                if cnt < 5:
-                    cnt += 1
-                else:
-                    return k, crossings
+                # if cnt < 5:
+                #     cnt += 1
+                # else:
+                return k, crossings
             k -= step
 
         return None, None
-    
+
+    @staticmethod
     def find_troughs(waveform, crossing_indices):
-        # left_vals = waveform[np.clip(crossing_indices - 1, 0, len(waveform) - 1)]
-        # right_vals = waveform[np.clip(crossing_indices + 1, 0, len(waveform) - 1)]
-        # direction = np.where(right_vals < left_vals, 1, -1)
-        assert len(crossing_indices) % 2 == 0
+        assert len(crossing_indices) % 2 == 0, f"Number of crossings must be even, got {len(crossing_indices)}"
         
         # make direction alternate between -1 and 1
         direction = np.tile([-1, 1], len(crossing_indices) // 2 + 1)[:len(crossing_indices)]
@@ -236,9 +236,9 @@ class IntergratedSplit(Segment):
 
         return trough_indices
 
-
     def __call__(self, data: torch.Tensor) -> List[torch.Tensor]:
-        integrated_waveform = np.sum(np.abs(data), axis=1)
+        data_np = data.numpy().copy()
+        integrated_waveform = np.sum(np.abs(data_np), axis=1)
         detrend_waveform = detrend(integrated_waveform)
         filtered_waveform = self.lowpass_filter(detrend_waveform)
         scaler = MinMaxScaler(feature_range=(0, 1))
@@ -246,11 +246,13 @@ class IntergratedSplit(Segment):
         k, crossings = self.find_k(scaled_waveform)
         
         if k is None:
+            warnings.warn("IntegratedSplit: fallback to Yungan method due to insufficient swings", UserWarning)
             # fallback to yungan's method
             return Yungan()(data)
         
         trough_indices = self.find_troughs(scaled_waveform, crossings)
-        segs = [(s, e) for s, e in zip(trough_indices[::2], trough_indices[1::2])]
+        
+        segs = [(s.item(), e.item()) for s, e in zip(trough_indices[::2], trough_indices[1::2])]
         
         return segs
 
